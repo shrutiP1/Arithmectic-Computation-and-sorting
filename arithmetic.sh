b24caf2a0755e198c1f9e5e9cf18436843edd906
shrutiP1 <shrutipatil13798@gmail.com>
2021-04-21 15:23:28 +0530

    Compute value of expression1

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,5 +1,12 @@
 echo "welcome "
 
+#UC1
+
 read -p "enter 1st value " a
 read -p "enter 2nd value " b
 read -p "enter 3rd value " c
+
+#UC2
+
+res1=$(($a+$b*$c))
+echo "output of expression is "$res1
